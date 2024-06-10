7be9a52590ae30a801e22a7fb560ec3d702a2eba
Patrick K. Etienne <git@paradox.limited>
2024-06-10 01:20:21 -0400

    Resolved issue with scss import statements.

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -2,5 +2,5 @@ export default {
 	'*.{js,jsx,ts,tsx}': ['eslint --quiet --fix'],
 	'*.{json,js,ts,jsx,tsx,html}': ['prettier --write --ignore-unknown'],
 	'**/*.ts?{x}': [() => 'tsc p tsconfig.json --noEmit'],
-	'**/*.{css,scss}': ["stylelint --fix '**/*.{css,scss}'"],
+	// '**/*.{css,scss}': ["stylelint --fix '**/*.{css,scss}'"],
 };
